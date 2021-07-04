f52f16cf74cb50acdaeb0831ef8092a5888c1a71
Satoru Tanaka <ittimfn@gmail.com>
2021-07-04 13:12:14 +0900

    Enumで持っている値を取得する

--- app/app.py
+++ app/app.py
@@ -3,9 +3,12 @@ import sys
 sys.path.append('./')
 from Color import Color
 from SampleEnum import SampleEnum
+from string_enum import StringEnum
 
 if __name__ == '__main__':
   print(Color.RED.getId())
 
   sample_enum = SampleEnum.value_of('HOGE')
   print(sample_enum.getKeys())
+
+  print(StringEnum.HOGE.value)
---
+++ app/string_enum.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+from enum import Enum
+
+class StringEnum(Enum):
+  HOGE = 'hoge'
